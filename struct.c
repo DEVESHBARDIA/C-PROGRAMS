144b65910a2785ee146987022a0d48fbb18a64c0
Rabbit-Bunny <108572863+Rabbit-Bunny@users.noreply.github.com>
2022-07-29 15:09:39 +0530

    Add files via upload

--- struct.c
+++ struct.c
@@ -4,6 +4,7 @@ struct student
 	int rollno;
 	char name[20];
 	float m1,m2,m3;
+	float avg;
 };
 
 int main()
@@ -13,17 +14,31 @@ int main()
 	scanf("%d",&n);
 	struct student s1[n];
 	
-	printf("Enter the details");
+	printf("Enter the details :\n");
 	for(int i=0;i<n;i++)
 	{
 	
-	printf("Enter rollno");
+	printf("Enter rollno :\t");
 	scanf("%d",&s1[i].rollno);
-	printf("Enter name");
-	scanf("%s",s1[i].name);
-	printf("Enter marks");
+	printf("Enter name :\t");
+	scanf(" %s",s1[i].name);
+	printf("Enter marks :\t");
 	scanf("%f%f%f",&s1[i].m1,&s1[i].m2,&s1[i].m3);
+	s1[i].avg = (s1[i].m1+s1[i].m2+s1[i].m3)/3.0;
 	}
-
+	printf("Students who scored above average is :\n");
+	printf("NAME\t ROLL_NUMBER\t AVERAGE\n");
+	for(int i=0; i<n; i++)
+	{
+	if (s1[i].avg>35)
+		{
+			printf("%s\t\t%d\t\t%f\n",s1[i].name,s1[i].rollno,s1[i].avg);
+		}
+	else 
+	{
+		printf("Students who scored below average is :\n");
+		printf("%s\t\t%d\t\t%f\n",s1[i].name,s1[i].rollno,s1[i].avg);
+	}
+	}
 return 0;
 }
