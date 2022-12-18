81bab5eda7c6e71cbd4db4b39653739a15fa01d9
DEVESH BARDIA <108572863+DEVESHBARDIA@users.noreply.github.com>
2022-12-18 10:39:40 +0530

    Update Array_operation.c

--- Array_operation.c
+++ Array_operation.c
@@ -1,71 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-int n=6;
-int pos;
-int a[100]={10,20,30,40,50,60,70};
-int insert_element();
-int delete_element();
-int display_array();
 
-int insert_element()
+int n,i,a[100],ele,pos;
+void create()
 {
-	int ele;
-	printf("Enter the element to be inserted: ");
-	scanf("%d",&ele);
-	printf("Enter the position where you want to insert: ");
-	scanf("%d",&pos);
-	for (int i=n;i>=pos;i--)
-	{
-		a[i] = a[i-1];
-	}
-	a[pos-1] = ele;
-	n = n + 1;
-	printf("Hurray Element inserted!");
+  printf("Enter the number of elements: ");
+  scanf("%d",&n);
+  printf("Enter the elements: ");
+  for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+  printf("Array created!");
 }
 
-int delete_element()
+void display()
 {
-	int ele;
-	printf("Enter the position to be deleted: ");
-	scanf("%d",&pos);
-	int temp = a[pos-1];
-	for(int i=pos-1;i<n-1;i++)
-	{
-		a[i] = a[i+1];
-	}
-	n = n - 1;
+  printf("The array is: \n");
+  for(i=0;i<n;i++)
+    printf("%d\n",a[i]);
+}
+
+void insert()
+{
+  printf("Enter the element you want to insert: ");
+  scanf("%d",&ele);
+  printf("Enter the position you want to insert: ");
+  scanf("%d",&pos);
+  for(i=n-1;i>=pos;i--)
+    {
+       a[i+1]=a[i]; 
+    }
+  a[pos] = ele;
+  n = n+1;
+}
+
+void delete()
+{
+  printf("Enter the position you want to delete: \n");
+  scanf("%d",&pos);
+  for(i=pos;i<n-1;i++)
+    {
+       a[i]=a[i+1]; 
+    }
+  n = n-1;
 }
 
 
-int display_array()
-{
-	printf("The array elements are:\n");
-	for (int i=0;i<n;i++)
-	{
-		printf("%d\n",a[i]);
-	}
-	return 0;
-}
 int main()
 {
-	int choice;
-	while (1) 
-	{
-		printf("\n---------------------- Menu------------------------\n");
-		printf("1.Insert an element\n2.Delete an element\n3.Display the Array\n4.Exit\n");
-		printf("Enter the choice you want to Perform:\n");
-		scanf("%d",&choice);
-		switch (choice)
-		{
-			case 1: insert_element();
-					break;
-			case 2: delete_element();
-					break;
-			case 3: display_array();
-					break;
-			case 4: exit(0);
-					break;
-			default: printf("Invalid Choice!");
-		}
-	}
+  int ch;
+  while(1)
+    {
+      printf("1.Create\n");
+      printf("2.Dislay\n");
+      printf("3.Insert\n");
+      printf("4.Delete\n");
+      printf("5.Exit\n");
+      printf("Enter your choice: ");
+      scanf("%d",&ch);
+      switch(ch)
+        {
+          case 1: create();
+                  break;
+          case 2: display();
+                  break;
+          case 3: insert();
+                  break;
+          case 4: delete();      
+                  break;
+          case 5: exit(0);
+                  break;
+          default: printf("Invalid choice!");
+        }
+    }
+  return 0;
 }
